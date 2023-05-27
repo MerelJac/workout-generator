0eca44dbc5dcded13f7fa5beb091e9595e8472e7
Merel Jacobs <merel.burleigh@gmail.com>
2023-05-27 11:58:55 -0700

    you can now edit, print to page, and regenerate the title of the workout

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,11 @@ var lowerBodyCheckbox = document.querySelector("#lowerbody");
 var regenerateBtn = document.querySelector("#regenerate-workout");
 var saveWorkout = document.querySelector("#save-workout");
 var addBtn = document.querySelector("#add-button");
+var workoutName = document.querySelector("#name");
+var newNameInputBox = document.querySelector("#nameInput");
+var dateSection = $("#workout-date");
+var saveTitleBtn = $("#saveTitleBtn");
+var editTitleSection = $("#editTitle");
 
 var exercises = ['Push-Ups', 'Pull-Ups', 'Sit Ups', 'Plank', 'Squats', 'Deadlifts', 'Rear Foot Elevated Split Squats', 'Calf Raises', 'Jog', 'Chest Press', 'Bicep Curls', 'Single Leg Deadlifts', 'Fire Hydrants', 'Donkey Kicks', 'Tricep Extensions', 'Hip Bridges', 'Hip Thrusts', 'Pistol Squats'];
 
@@ -23,19 +28,49 @@ function buildWorkout() {
     var savedSection = $('#saved-section');
     savedSection.hide();
     for (var i = 0; i < workoutLength; i++) {
+        // create a li in the printed section
         var li = document.createElement("li");
+        // the HTML text is whatever the exercise[i] is
         li.innerText = JSON.stringify(exercises[i]);
+        // randomly selectes
         var pickRandom = exercises[Math.floor(Math.random() * exercises.length)];
+        // won't print duplicates
         if (!includeWorkouts.includes(pickRandom)) {
             includeWorkouts.push(pickRandom)
-        }
-    };
-    localStorage.setItem("exerciseArray", []);
+        }};
     includeWorkouts.forEach(function (li) {
         document.querySelector("#print-here").innerHTML += `<div class="alert alert-light alert-dismissible fade show" role="alert" id="exerciseToday">${li}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
-        localStorage.setItem("exercise", [li]);
     })};
 
+function editName() {
+    workoutName.setAttribute("style", "display: none;");
+    var editTitleDiv = $('#editTitle');
+    var newTitleInput = document.createElement("input");
+    newTitleInput.setAttribute("id", "titleInput");
+    var saveTitleBtn = document.createElement("button");
+    saveTitleBtn.setAttribute("id", "saveTitleBtn");
+    saveTitleBtn.innerText = "Save Title";
+    editTitleDiv.append(newTitleInput);
+    editTitleDiv.append(saveTitleBtn);
+    saveTitleBtn.addEventListener("click", function() {
+        var titleText = newTitleInput.value;
+        var newTitleh2 = document.createElement("h2");
+        newTitleh2.setAttribute("id", "titleH2");
+        newTitleInput.setAttribute("id", "newTitle");
+        newTitleh2.textContent = titleText;
+        editTitleDiv.append(newTitleh2);
+        var saveTitleBtn = $("#saveTitleBtn");
+        saveTitleBtn.css("display", "none");
+        var titleTextBox = $("#newTitle");
+        titleTextBox.css("display", "none");
+    });
+};
+
+function saveNewName() {
+    var newNameInputBox = $("#titleInput");
+    var newName = newNameInputBox.textContent;
+    editTitleSection.innerHTML += `<h2>${newName}</h2>`
+}
 
 $(function(){
     var exercisePrintedList = $('#print-here');
@@ -53,6 +88,10 @@ function regenerate() {
     $("#print-here").empty();
     buildWorkout();
     localStorage.clear("exerciseArray");
+    workoutName.setAttribute("style", "display: flex;");
+    var newH2 = $("#titleH2");
+    newH2.css("display", "none");
+
 };
 
 
@@ -63,6 +102,7 @@ var pastWorkoutsArray = JSON.parse(localStorage.getItem('savedWorkouts')) || [];
 // not working yet
 var savedWorkout = [];
 function saveToFiles() {
+    var exercisesIncluded = document.querySelectorAll()
     var workoutObject = {
         date: today,
         exercises: exerciseInput.values
@@ -71,6 +111,7 @@ savedWorkout.push(workoutObject);
 localStorage.setItem("savedWorkout", JSON.stringify(savedWorkout));
 console.log("check");
 };
+
 
 function showSaved() {
     var savedSection = $('#saved-section');
@@ -113,15 +154,6 @@ $( function() {
       source: availableTags
     });
   } );
-    // var givenExercise = document.querySelector("#print-here").children;
-    // console.log(givenExercise);
-    // var workoutObject = {
-    //     name: "Workout of the day",
-    //     date: today,
-    //     exercises: givenExercise
-    // };
-    // pastWorkoutsArray.push(workoutObject);
-    // localStorage.setItem("pastWorkouts", JSON.stringify(pastWorkoutsArray))};
 
 buildWorkoutBtn.addEventListener("click", buildWorkout);
 savedSectionBtn.addEventListener("click", showSaved);
@@ -129,3 +161,6 @@ regenerateBtn.addEventListener("click", regenerate);
 saveWorkout.addEventListener("click", saveToFiles);
 generateWorkoutBtn.addEventListener("click", buildWorkout);
 addBtn.addEventListener("click", addItem);
+workoutName.addEventListener("click", editName);
+
+
